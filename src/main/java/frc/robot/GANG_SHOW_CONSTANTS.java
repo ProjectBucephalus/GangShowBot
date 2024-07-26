66e9b120ea0f9037b6261f54acdd479ab3b9705b
Adam <125779905+SebastianAiello@users.noreply.github.com>
2024-07-27 01:41:43 +1000

    Added comments

--- src/main/java/frc/robot/GANG_SHOW_CONSTANTS.java
+++ src/main/java/frc/robot/GANG_SHOW_CONSTANTS.java
@@ -4,20 +4,37 @@ package frc.robot;
 public class GANG_SHOW_CONSTANTS 
 {
 
+    // ----------------------- //
+    // > How the robot moves < //
+    // ----------------------- //
+
     /** Maximum robot speed in metres per second */
-    public static final double maxSpeed = 0.5; 
+    public static final double maxSpeed = 0.5;
+
     /** Maximum robot rotation speed in radians per second (6 radians is about 1 rotation) */
     public static final double maxAngularVelocity = 3;
+
     /** How fast to twist the wheels to a new direction, increase if changing direction is sluggish */
     public static final double wheelTwistRate = 12;
 
-    // >>> Geo-fence setup is not implimented yet <<< //
+
+    // -------------------------- //
+    // > Where the robot can go < //
+    // -------------------------- //
+
+    // =================================== //
+    // >>> This part is not set up yet <<< //
+    // =================================== //
+
     /** Metres the robot can travel left */
     public static final double stageLeft = -1;
+
     /** Metres the robot can travel right */
-    public static final double stageRight = 10; 
+    public static final double stageRight = 10;
+
     /** Metres the robot can travel forwards */
-    public static final double stageFront = 4;     
+    public static final double stageFront = 4;
+
     /** Metres the robot can travel back */
     public static final double stageBack = -1;    
 
